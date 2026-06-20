6d5d02a30fb19226a99b804d2260fcdc9f57f3ba
Alusovvy <aleksanderzawisza258@gmail.com>
2026-06-20 12:04:14 +0200

    Basic command handling and basic tests

--- src/server.c
+++ src/server.c
@@ -12,31 +12,69 @@
 #include <poll.h>
 #include <errno.h>
 
-#include <poll.h>
-
 #define HOST "127.0.0.1"
 #define DEFAULT_PORT "9090"
 #define BACKLOG 10 // how many pending connections queue holds
+
+typedef enum {
+	LOGIN_COMMAND,
+	MESSAGE_HISTORY,
+	JOIN_CHAT,
+	CREATE_CHAT,
+	SHOW_DETAILS,
+	UNKNOWN
+} ChatCommand;
+
+
+struct s_message {
+	
+	char username[64];
+	char content[512];
+
+}; // TO-DO: Expand this later on to Hash-Table
+
+typedef struct s_message Message;
+
+struct s_chat_room {
+	char name[64];
+	struct s_message* messages[512];
+	//add structure that holds details about chat users
+};
+
+typedef struct s_chat_room Chatroom;
 
 struct s_client {
 	int fd;
 	int port;
 	char ip[INET_ADDRSTRLEN];
 	char name[64];
+	bool isLoggedIn;
+	Chatroom* activeChat;
+	
+	struct s_message messages[128];
 	struct sockaddr_in c_sockaddr;
 };
 
-struct s_message {
-	
-	char* username;
-	char* message;
-
-}; // TO-DO: Expand this later on to Hash-Table
-
 typedef struct s_client Client;
 
-
 bool isRunning= true;
+
+ChatCommand parseCommand(char* userInput) {
+	printf("Raw command from user: %s\n", userInput);
+	if  (strncmp(userInput, "/login", strlen("/login")) == 0){
+		return LOGIN_COMMAND;
+	} else if (strncmp(userInput, "/history", strlen("/history")) == 0) {
+		return MESSAGE_HISTORY;
+	} else if (strncmp(userInput, "/join", strlen("/join")) == 0) {
+		return JOIN_CHAT;
+	} else if (strncmp(userInput, "/create", strlen("/create")) == 0) {
+		return CREATE_CHAT;
+	} else if (strncmp(userInput, "/details", strlen("/details")) == 0) {
+		return SHOW_DETAILS;
+	}
+
+	return UNKNOWN;
+}
 
 int init_server(unsigned int port) {
 
@@ -85,11 +123,76 @@ void addClientToList(Client* client, Client* client_list[]) {
     	}
 }
 
-void requestUsername(Client* client) {
+void handleLogin(Client* client, char* buf) {
+	//offszotuje login komende
+	char* username = buf + strlen("/login ");
+	printf("%s\n", client->name);
+	if (client->name[0] == '\0') {
+		strncpy(client->name, username, sizeof(client->name) - 1);
+		client->name[strcspn(client->name, "\r\n")] = '\0';
+		client->isLoggedIn = true;
+		char msg[128];
+		int len = snprintf(msg, sizeof(msg), "User was logged in with username: %s\n", username);
+		send(client->fd, msg, len, 0);
+		printf("%s\n",msg);
+	}
+}
 
-	char buf[128] = "Podaj nazwe użytkownika\n";
+void displayCommands(Client* client) {
+	char buf[128] = "Type /login to login with your username\n";
 	send(client->fd, buf, strlen(buf), 0);
 }
+
+Chatroom* createChatroom(Chatroom* chatrooms[], char* buf) {
+	char* name = buf + strlen("/login ");
+	//find empty space for chatroom
+	for(int i = 0; i<10; i++) {
+		if (chatrooms[i]->name[0] == '\0') {
+			chatrooms[i] = malloc(sizeof(struct s_chat_room));
+			memset(chatrooms[i], 0, sizeof(struct s_chat_room));
+			strncpy(chatrooms[i]->name, name, sizeof(chatrooms[i]->name) - 1);
+			printf("Chatroom created with name: %s\n", name);
+			return chatrooms[i];
+			break;
+		}
+	}
+
+	printf("No space for new chatroom, wait");
+	return NULL;
+
+}
+
+void joinChat(Chatroom* chatrooms[], Client* client, char* buf) {
+	char* chatroom_name = buf + strlen("/join ");
+	printf("Chatroom to join: %s\n", chatroom_name);
+	for (int i = 0; i<10; i++) {
+		
+		if (strncmp(chatrooms[i]->name, chatroom_name, strlen(chatrooms[i]->name)) == 0 ) {
+			client->activeChat = chatrooms[i];
+			break;
+		}
+
+	}
+
+}
+
+void sendMessageToChat(Client* client, char* buf) {
+	char* username = client->name;
+	Chatroom* chatroom = client->activeChat;
+
+	for (int i = 0; i<128; i++) {
+		
+		if (chatroom->messages[i] == NULL) {
+			Message* message = malloc(sizeof(struct s_message));
+			strncpy(message->username, username, sizeof(message->username) - 1);
+			strncpy(message->content, buf, sizeof(message->content) - 1);
+			chatroom->messages[i] = message;
+		}
+
+	}
+	
+}
+
 
 Client* createClientSocketAsync(int fd) {
 	//client TCP config and connection accept
@@ -108,18 +211,26 @@ Client* createClientSocketAsync(int fd) {
 	Client* client_s = (Client *)malloc(sizeof(struct s_client)); // mozliwe ze bedzie trzeba wyzerowac
 	memset(client_s, 0, sizeof(struct s_client));
 	client_s->fd = accept(fd, (struct sockaddr *)&client, &len); 
+	memset(client_s->name, 0, sizeof(client_s->name)); 
 
 	client_s->c_sockaddr = client;
 	int port = ntohs(client.sin_port);
 	client_s->port = port;
 	inet_ntop(AF_INET, &client.sin_addr, client_s->ip, sizeof(client_s->ip));
 	printf("New connection from %s:%d\n", client_s->ip, port);
-	requestUsername(client_s);
+	displayCommands(client_s);
 	return client_s;
 }
 
+void sendMessageToClient(Client* client, char* message) {
+	char msg[128];
+	int len = snprintf(msg, sizeof(msg), "%s\n", message);
+	send(client->fd, msg, len, 0);
 
-void handleClient(Client* client_list[], int i) {
+}
+
+
+void handleClient(Chatroom* chatrooms[], Client* client_list[], int i) {
 	//getting a message
 	char buf[512] = {0};
 	int conn = recv(client_list[i]->fd, buf, 511, 0);
@@ -127,12 +238,46 @@ void handleClient(Client* client_list[], int i) {
 	//check clients for message
 	
 	if (conn > 0) {
-		if (client_list[i]->name[0] == '\0') {
-
-			strncpy(client_list[i]->name, buf, sizeof(client_list[i]->name) - 1);
-			client_list[i]->name[strcspn(client_list[i]->name, "\r\n")] = '\0';
-			printf("Client set name: %s\n", client_list[i]->name);
+		if (client_list[i]->isLoggedIn) {
+			switch(parseCommand((char*)&buf[0])) {
+				case (MESSAGE_HISTORY): 
+					printf("Command was '/history'\n");
+					break;
+				case (JOIN_CHAT): 
+					printf("Command was '/join'\n");
+					joinChat(chatrooms, client_list[i], buf);
+					break;
+				case (CREATE_CHAT): 
+					printf("Command was '/create'\n");
+					createChatroom(chatrooms, buf);
+					break;
+				case (SHOW_DETAILS):
+					printf("Command was '/details'\n");
+					sendMessageToClient(client_list[i], client_list[i]->name);
+					break;
+				case (UNKNOWN):
+					if (client_list[i]->activeChat) {
+						sendMessageToChat(client_list[i], buf);
+					}
+					printf("Command was UNKNOWN\n");
+					break;
+			}
+		} else {
+			switch(parseCommand((char*)&buf[0])) {
+				case (LOGIN_COMMAND): 
+					printf("Command was '/login'\n");
+					handleLogin(client_list[i], buf);
+					break;
+				case (MESSAGE_HISTORY):
+				case (JOIN_CHAT): 
+				case (CREATE_CHAT):
+				case (SHOW_DETAILS):
+				case (UNKNOWN):
+					printf("User no logged in!\n");
+					send(client_list[i]->fd, "User not logged in\n", strlen("User not logged in"), 0);
+			}
 		}
+	
 	} else if (conn == 0 || errno != EINTR) {
 		printf("Client disconnected: %s\n", client_list[i]->ip);
 		close(client_list[i]->fd);
@@ -143,7 +288,8 @@ void handleClient(Client* client_list[], int i) {
 }
 
 
-void main_loop(int fd, Client* client_list[]) {
+void main_loop(int fd, Client* client_list[], Chatroom* chatrooms[]) {
+	
 	struct pollfd fds[11]; // as I will server 10 clients + my server socket
 	memset(fds, 0, sizeof(fds));
 	
@@ -172,20 +318,22 @@ void main_loop(int fd, Client* client_list[]) {
 	}
 
 	for (int j = 1; j < nfds; j++) {
-		if (fds[j].events & POLLIN) {
-
-			handleClient(client_list, client_index[j - 1]);
+		if (fds[j].revents & POLLIN) {
+			handleClient(chatrooms, client_list, client_index[j - 1]);
 		}
 	}
 }
+
 /* TO-DO: Add htons or other combination to convert from network byte order to host or vice versa */
 int main(int argc, char *argv[])
 {
 	Client* client_list[10] = {0};
+	Chatroom* chatrooms[12] = {0};
 	unsigned int port;
 	int socket_file_descriptor;
 	
 	memset(client_list, 0, sizeof(client_list));
+	memset(chatrooms, 0, sizeof(chatrooms));
 
 	if (argc < 2) {
 	 printf("Port NOT provided, defaulting to %s \n", DEFAULT_PORT);
@@ -199,7 +347,7 @@ int main(int argc, char *argv[])
 	socket_file_descriptor = init_server(port);
 
 	while(isRunning) {
-		main_loop(socket_file_descriptor, client_list);
+		main_loop(socket_file_descriptor, client_list, chatrooms);
 	}
 
 	close(socket_file_descriptor);
--- tests/test.sh
+++ tests/test.sh
@@ -1,21 +1,62 @@
 #!/bin/bash
 
-MESSAGE="My Test Message"
+SERVER="127.0.0.1"
+PORT="9090"
+PASS=0
+FAIL=0
 
-connect() {
-    local i=$1
-    echo "Test $i"
-    
-    if echo "$MESSAGE" | nc -w 3 127.0.0.1 9090 2>/dev/null; then
-        echo "Terminal $i: Połączono pomyślnie"
-    else
-        echo "Terminal $i: Błąd połączenia"
-   fi
+normalize(){
+    tr -d ' \t\r\n' |
+    sed 's/^[[:space:]]*//;s/[[:space:]]*$//' |
+    grep -v '^$'
 }
 
-for i in $(seq 1 5); do
-    connect $i &
-done
+send() { printf "%s\n" "$1" >&3; }
+recv() { 
+    local line
+    while read -t0.5 -r line <&3; do
+        echo "$line"
+    done
+}
+test() {
+    [ -f /tmp/got.txt ] && rm /tmp/got.txt
+    [ -f /tmp/expected.txt ] && rm /tmp/expected.txt
+    local isLoggedIn=$1
+    local name="$2"
+    local input="$3"
+    local expected="$4"    
+    
+    exec 3<>/dev/tcp/$SERVER/$PORT
 
-wait
-echo "--- Wszystkie połączenia zakończone ---"
+    if [ $isLoggedIn == true ]; then
+        send "/login alex"
+        recv
+    fi
+
+    send "$input"
+    recv >> /tmp/got.txt
+    echo $expected > /tmp/expected.txt
+
+    exec 3>&- 3<&-
+    
+    normalize < /tmp/got.txt > /tmp/got_norm.txt
+    normalize < /tmp/expected.txt > /tmp/expected_norm.txt
+
+    if diff -q /tmp/expected_norm.txt /tmp/got_norm.txt > /dev/null; then
+        echo "PASS: $name"
+        PASS=$((PASS + 1))
+    else 
+    echo "FAIL: $name"
+    echo " expected: 
+        $(cat /tmp/expected_norm.txt)
+        "
+    echo " got: 
+        $(cat /tmp/got_norm.txt)
+        "
+    FAIL=$((FAIL + 1))
+    fi
+}
+
+    test false "Login" "/login aleks" "Type /login to login with your username User was logged in with username: aleks"
+    test true "Get Details" "/details" "alex"
+   # test true "Create Chatroom" "/create room" 
